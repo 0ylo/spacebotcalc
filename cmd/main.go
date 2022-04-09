6de136c07064d4aec01bc61140d7bd979b915e4d
0ylo <l16.rid@gmail.com>
2022-04-09 23:08:30 +0300

    initial commit

--- cmd/main.go
+++ cmd/main.go
@@ -10,10 +10,6 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-const (
-//webhook = "https://spacebotcalc.herokuapp.com/"
-)
-
 var (
 	// глобальная переменная в которой храним токен
 	telegramBotToken string
