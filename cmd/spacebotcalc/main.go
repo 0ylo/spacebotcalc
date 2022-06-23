7f0ca18a47c249dae986fed3a98c1eb19d1145f8
Anton <96187255+0ylo@users.noreply.github.com>
2022-06-23 07:53:34 +0300

    Start new branch

--- cmd/spacebotcalc/main.go
+++ cmd/spacebotcalc/main.go
@@ -2,20 +2,35 @@ package main
 
 import (
 	"log"
+	"strconv"
 
+	"github.com/0ylo/spacebotcalc/internal/commands"
 	"github.com/0ylo/spacebotcalc/internal/config"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
+type DataCalc struct {
+	State    int //1-Deposit, 2-Duration
+	Name     string
+	Deposit  string
+	Duration string
+}
+
 var (
-	version string = "unknown"
-	build   string = "unknown"
+	dataCalcMap map[int64]*DataCalc
+	dc          *DataCalc
+	version     string = "unknown"
+	build       string = "unknown"
 )
 
-var numericKeyboard = tgbotapi.NewReplyKeyboard(
+func init() {
+	dataCalcMap = make(map[int64]*DataCalc)
+}
+
+var mainMenu = tgbotapi.NewReplyKeyboard(
 	tgbotapi.NewKeyboardButtonRow(
-		tgbotapi.NewKeyboardButton("Calculate"),
-		tgbotapi.NewKeyboardButton("More info"),
+		tgbotapi.NewKeyboardButton("💰 Calculate"),
+		tgbotapi.NewKeyboardButton("📢 More info"),
 	),
 )
 
@@ -51,41 +66,89 @@ func main() {
 
 	// Reed a "update" messeges from "updates" channel
 	for update := range updates {
+
+		// ignore any non-Message updates
 		if update.Message == nil {
 			continue
 		}
 
-		// Loging - from whom what message came
-		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+		// For commands
+		if update.Message.IsCommand() {
+			cmdText := update.Message.Command()
+			if cmdText == "start" {
+				msgConfig := tgbotapi.NewMessage(
+					update.Message.Chat.ID,
+					"Hello! I help you to calculate your profit by staking in SpaceBot Project")
+				msgConfig.ReplyMarkup = mainMenu
+				bot.Send(msgConfig)
+			} else if cmdText == "info" {
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Please, reed this instructions - https://teletype.in/@arousal/spacebot")
+				bot.Send(msg)
+			}
+		} else {
+			// For messages
 
-		// Create answer message
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+			if update.Message.Text == mainMenu.Keyboard[0][0].Text {
 
-		// Default answer for any message
-		msg.Text = "Sorry, unknown command. Just use butttons bellow, or type <Calculate> or <More Info>"
+				dataCalcMap[update.Message.From.ID] = new(DataCalc)
+				dataCalcMap[update.Message.From.ID].State = 1
 
-		// TG keyboard
-		msg.ReplyMarkup = numericKeyboard
+				msgConfig := tgbotapi.NewMessage(update.Message.Chat.ID, "Введите, пожалуйста, сумму депозита 👇")
+				bot.Send(msgConfig)
+			} else {
+				dc, ok := dataCalcMap[update.Message.From.ID]
+				if ok {
+					if dc.State == 1 {
+						dc.Deposit = update.Message.Text
+						msgConfig := tgbotapi.NewMessage(update.Message.Chat.ID, "Введите, пожалуйста, срок депозита 👇")
+						bot.Send(msgConfig)
+						dc.State = 2
+					} else if dc.State == 2 {
+						dc.Duration = update.Message.Text
 
-		// Switch for commands from bot, wich starts from "/"
-		switch update.Message.Command() {
-		case "start":
-			msg.Text = "Hello! I help you to calculate your profit by staking in SpaceBot Project"
-		case "calc":
-			msg.Text = "Function in progress..."
-		case "info":
-			msg.Text = "Please, reed this instructions - https://teletype.in/@arousal/spacebot"
+						// ----
+						de, err := strconv.ParseFloat(dc.Deposit, 64)
+						log.Printf("Convert f, g:", de, err)
+						du, err := strconv.Atoi(dc.Duration)
+						log.Printf("Convert f, g:", du, err)
+
+						res1, res2 := commands.Calculate(de, du)
+						log.Print("res1&2 is ", res1, res2)
+						// ----
+
+						msgConfig := tgbotapi.NewMessage(update.Message.Chat.ID, "Считаю...")
+						bot.Send(msgConfig)
+						delete(dataCalcMap, update.Message.From.ID)
+					}
+				} else {
+					// other messages
+					if update.Message.Text != mainMenu.Keyboard[0][1].Text {
+						msgConfig := tgbotapi.NewMessage(update.Message.Chat.ID, "Нет такой команды, нажмите, пожалуйста, на одну из кнопок ниже👇")
+						bot.Send(msgConfig)
+					}
+				}
+			}
+
+			if update.Message.Text == mainMenu.Keyboard[0][1].Text {
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Please, reed this instructions - https://teletype.in/@arousal/spacebot")
+				bot.Send(msg)
+			}
 		}
 
-		// Switch for buttons
-		switch update.Message.Text {
-		case "Calculate":
-			msg.Text = "Coming soon"
-		case "More info":
-			msg.Text = "Please, reed this instructions - https://teletype.in/@arousal/spacebot"
-		}
+		/*
+			// Switch for commands from bot, wich starts from "/"
+			switch update.Message.Command() {
+			case "start":
+				msgConfig := tgbotapi.NewMessage(
+					update.Message.Chat.ID,
+					"Hello! I help you to calculate your profit by staking in SpaceBot Project")
+				msgConfig.ReplyMarkup = mainMenu
+				bot.Send(msgConfig)
+			case "info":
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Please, reed this instructions - https://teletype.in/@arousal/spacebot")
+				bot.Send(msg)
+			}
+		*/
 
-		// Send message
-		bot.Send(msg)
 	}
 }
